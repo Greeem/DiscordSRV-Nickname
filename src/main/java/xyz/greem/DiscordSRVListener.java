83c6eeb4891e4cd8208955bea74167d038aa3eab
Greem Husted <gr33m11@gmail.com>
2018-07-18 16:32:16 -0500

    extra shit

--- src/main/java/xyz/greem/DiscordSRVListener.java
+++ src/main/java/xyz/greem/DiscordSRVListener.java
@@ -3,15 +3,42 @@ package xyz.greem;
 import github.scarsz.discordsrv.DiscordSRV;
 import github.scarsz.discordsrv.api.Subscribe;
 import github.scarsz.discordsrv.api.events.AccountLinkedEvent;
+import github.scarsz.discordsrv.dependencies.jda.core.entities.Member;
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.bukkit.event.player.PlayerLoginEvent;
 
-public class DiscordSRVListener {
+public class DiscordSRVListener implements Listener {
 
     @Subscribe
     public void accountsLinked(AccountLinkedEvent event) {
-
-        Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "nick " + event.getPlayer().getName() + " " + DiscordSRV.getPlugin().getMainGuild().getMember(event.getUser()).getEffectiveName());
-
+        Bukkit.dispatchCommand( Bukkit.getConsoleSender(), "nick " +
+                event.getPlayer().getName() + " " +
+                DiscordSRV.getPlugin().getMainGuild().getMember( event.getUser() ).getEffectiveName() );
     }
 
+    @EventHandler
+    public void onLogin(PlayerLoginEvent event) {
+        if (!DiscordSRV.getPlugin().getAccountLinkManager().getLinkedAccounts().containsValue(event.getPlayer().getUniqueId())) {
+            event.getPlayer().sendRawMessage(ChatColor.GREEN +
+                    "Please link your Discord account to your Minecraft account via " +
+                    ChatColor.AQUA + "/discord link" +
+                    ChatColor.GREEN + ", and follow the on-screen instructions.");
+        }
+
+        Member discordUser = DiscordSRV.getPlugin().getMainGuild().getMemberById(DiscordSRV.getPlugin().getAccountLinkManager().getDiscordId(event.getPlayer().getUniqueId()));
+        Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "nick " + event.getPlayer().getName() + " " +
+                discordUser.getEffectiveName());
+    }
+
+    @EventHandler
+    public void onMessageSend(AsyncPlayerChatEvent event) {
+        if (!DiscordSRV.getPlugin().getAccountLinkManager().getLinkedAccounts().containsValue(event.getPlayer().getUniqueId())) {
+            event.getPlayer().sendRawMessage(ChatColor.GREEN + "You cannot chat until you've linked your Discord account!");
+            event.setCancelled(true);
+        }
+    }
 }
--- src/main/java/xyz/greem/Plugin.java
+++ src/main/java/xyz/greem/Plugin.java
@@ -11,12 +11,23 @@ public final class Plugin extends JavaPlugin {
     public void onEnable() {
         getLogger().info("Plugin enabling!");
         DiscordSRV.api.subscribe(discordSRVListener);
+        init();
     }
 
     @Override
     public void onDisable() {
         getLogger().info("Plugin disabling!");
         DiscordSRV.api.unsubscribe(discordSRVListener);
+    }
+
+    public void init() {
+        getLogger().info("ChangeNickname - V1.1 by Greem#1337");
+        getLogger().info("hooks into DiscordSRV to change a user's nickname in-game");
+        getLogger().info("to their Discord nickname upon linking their accounts with DiscordSRV.");
+        getLogger().info("This plugin is entirely open source! Grab it here: https://github.com/Greeem/DiscordSRV-Nickname/");
+
+        getLogger().info("Made with love and very little Bukkit knowledge.");
+
     }
 
 
